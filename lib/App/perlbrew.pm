9177fd283e2e860a346c2c552ecc6717a6f636c6
Kang-min Liu <gugod@gugod.org>
2010-12-06 12:07:04 +0800

    regenerate perlbrew executable for 0.15.

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -5,7 +5,7 @@ use 5.008;
 use Getopt::Long ();
 use File::Spec::Functions qw( catfile );
 
-our $VERSION = "0.14_99";
+our $VERSION = "0.15";
 our $CONF;
 
 my $ROOT         = $ENV{PERLBREW_ROOT} || "$ENV{HOME}/perl5/perlbrew";
