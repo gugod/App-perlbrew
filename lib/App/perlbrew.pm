84d7e416a8d8aeee931f4476f966052d41f725f3
Jose Rey <reyjose@gmail.com>
2011-04-25 22:57:46 -0430

    rewrite the mirror command (run_command_mirror)
    Improvements:
    - Custom mirror (m command) now works
    - Ask for mirror after last page
    - Easier logic (less efficient)

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -701,33 +701,33 @@ sub run_command_mirror {
         }
     }
 
-    my $select;
     require ExtUtils::MakeMaker;
-    MIRROR: foreach my $id ( 0..$#mirrors ) {
-        my $mirror = $mirrors[$id];
-        printf "[% 3d] %s\n", $id + 1, $mirror->{name};
-        if ( $id > 0 ) {
-            my $test = $id / 19;
-            if ( $test == int $test ) {
-                my $remaining = $#mirrors - $id;
-                my $ask = "Select a mirror by number or press enter to see the rest "
-                        . "($remaining more) [q to quit, m for manual entry]";
-                my $val = ExtUtils::MakeMaker::prompt( $ask );
-                next MIRROR if ! $val;
-                last MIRROR if $val eq 'q';
-                $select = $val;
-		if($select eq 'm') {
-                    my $url  = ExtUtils::MakeMaker::prompt("Enter the URL of your CPAN mirror:");
-		    my $name = ExtUtils::MakeMaker::prompt("Enter a Name: [default: My CPAN Mirror]") || "My CPAN Mirror";
-		    $select = { name => $name, url => $url };
-		}
-                elsif ( ! $select || $select - 1 > $#mirrors ) {
-                    die "Bogus mirror ID: $select";
-                }
-                $select = $mirrors[$select - 1] unless ($select eq 'm');
-                die "Mirror ID is invalid" if ! $select;
-                last MIRROR;
-            }
+    my $select;
+    my $max = @mirrors;
+    my $id  = 0;
+    while ( @mirrors ) {
+        printf "[% 3d] %s\n", ++$id, $_->{name} for splice(@mirrors,0,20);
+        my $remaining = $max - $id;
+        my $ask = "Select a mirror by number or press enter to see the rest "
+                . "($remaining more) [q to quit, m for manual entry]";
+        my $val = ExtUtils::MakeMaker::prompt( $ask );
+        if ( ! length $val )  { next }
+        elsif ( $val eq 'q' ) { last }
+        elsif ( $val eq 'm' ) {
+            my $url  = ExtUtils::MakeMaker::prompt("Enter the URL of your CPAN mirror:");
+            my $name = ExtUtils::MakeMaker::prompt("Enter a Name: [default: My CPAN Mirror]") || "My CPAN Mirror";
+            $select = { name => $name, url => $url };
+            last;
+        }
+        elsif ( not $val =~ /\s*(\d+)\s*/ ) {
+            die "Invalid answer: must be 'q', 'm' or a number\n";
+        }
+        elsif (1 <= $val and $val <= $max) {
+            $select = $mirrors[ $val - 1 ];
+            last;
+        }
+        else {
+            die "Invalid ID: must be between 1 and $max\n";
         }
     }
     die "You didn't select a mirror!\n" if ! $select;
