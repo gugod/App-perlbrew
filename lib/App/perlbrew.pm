ad1f8014842ee74a0a3b4a767c8a1f7b62cbf754
Kang-min Liu <gugod@gugod.org>
2011-07-10 01:46:22 +0800

    version up, and update changelog

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -6,7 +6,7 @@ use Getopt::Long ();
 use File::Spec::Functions qw( catfile );
 use FindBin;
 
-our $VERSION = "0.25";
+our $VERSION = "0.26";
 our $CONF;
 
 my $ROOT             = $ENV{PERLBREW_ROOT} || "$ENV{HOME}/perl5/perlbrew";
