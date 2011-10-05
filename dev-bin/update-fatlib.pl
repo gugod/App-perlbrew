af3bbcdeebb22ce7f6156338eec95668ffb3a875
Kang-min Liu <gugod@gugod.org>
2011-10-05 11:18:35 +0800

    steal upgrade-fatlib.pl from cpanminus project ;)

---
+++ dev-bin/update-fatlib.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use App::FatPacker ();
+use File::Path;
+use Cwd;
+
+my $modules = [ split /\s+/, <<MODULES ];
+local/lib.pm
+File/Path/Tiny.pm
+Text/Levenshtein.pm
+MODULES
+
+my $packer = App::FatPacker->new;
+my @packlists = $packer->packlists_containing($modules);
+$packer->packlists_to_tree(cwd . "/fatlib", \@packlists);
+
+use Config;
+rmtree("fatlib/$Config{archname}");
+
+use File::Find;
+File::Find::find({
+    wanted => sub {
+        /^.*\.pod\z/s && unlink;
+    } }, 'fatlib');
