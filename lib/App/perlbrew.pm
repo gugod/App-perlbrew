93337ea18e08c17e67ac27b6c6b7172bf16eddd9
Ilmari Vacklin <ilmari@dicole.com>
2011-10-26 14:55:33 +0300

    Quote parameters to export in __perlbrew_set_path
    This fixes #156.

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -60,13 +60,13 @@ __perlbrew_reinit () {
 __perlbrew_set_path () {
     [[ -n $(alias perl 2>/dev/null) ]] && unalias perl 2>/dev/null
 
-    export PATH_WITHOUT_PERLBREW=$(perl -e 'print join ":", grep { index($_, $ENV{PERLBREW_ROOT}) } split/:/,$ENV{PATH};')
+    export PATH_WITHOUT_PERLBREW="$(perl -e 'print join ":", grep { index($_, $ENV{PERLBREW_ROOT}) } split/:/,$ENV{PATH};')"
 
     if [[ -z "$PERLBREW_PATH" ]]; then
         export PERLBREW_PATH="$PERLBREW_ROOT/bin"
     fi
 
-    export PATH=$PERLBREW_PATH:$PATH_WITHOUT_PERLBREW
+    export PATH="$PERLBREW_PATH:$PATH_WITHOUT_PERLBREW"
 }
 __perlbrew_set_path
 
