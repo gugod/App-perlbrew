ee73330310bc10ae434734b5ee6c3256419f5954
Kang-min Liu <gugod@gugod.org>
2011-11-04 02:28:11 +0800

    version up

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -7,7 +7,7 @@ use File::Spec::Functions qw( catfile catdir );
 use File::Path::Tiny;
 use FindBin;
 
-our $VERSION = "0.30";
+our $VERSION = "0.31";
 our $CONFIG;
 
 our $PERLBREW_ROOT = $ENV{PERLBREW_ROOT} || "$ENV{HOME}/perl5/perlbrew";
