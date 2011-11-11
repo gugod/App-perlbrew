055853542c5b1e04b112a99713992ce18282af4b
Kang-min Liu <gugod@gugod.org>
2011-11-12 07:02:44 +0800

    pack local::lib and bump version

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -7,7 +7,7 @@ use File::Spec::Functions qw( catfile catdir );
 use File::Path::Tiny;
 use FindBin;
 
-our $VERSION = "0.31";
+our $VERSION = "0.32";
 our $CONFIG;
 
 our $PERLBREW_ROOT = $ENV{PERLBREW_ROOT} || "$ENV{HOME}/perl5/perlbrew";
