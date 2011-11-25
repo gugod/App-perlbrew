dfeda53f53a9cfa73cb2cb31a6bad0744410dc59
Kevin McGrath <kmcgrath@baknet.com>
2011-11-25 08:11:50 -0500

    don't blow away original PERL5LIB

--- dev-bin/build.sh
+++ dev-bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PERL5LIB=lib
+export PERL5LIB=lib:$PERL5LIB
 # fatpack trace bin/perlbrew
 # fatpack packlists-for `cat fatpacker.trace` > packlists
 # fatpack tree `cat packlists`
