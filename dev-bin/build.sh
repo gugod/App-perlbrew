c6b2f243ce96514b0613cc45ec0605bd3cad5766
Kang-min Liu <gugod@gugod.org>
2012-06-11 21:51:19 +0200

    minor message.

--- dev-bin/build.sh
+++ dev-bin/build.sh
@@ -7,7 +7,7 @@ mkdir -p lib/App
 if type perlstrip >/dev/null 2>&1; then
     perlstrip -o lib/App/perlbrew.pm ../lib/App/perlbrew.pm
 else
-    echo "+++ perlstrip is not installed."
+    echo "!!! perlstrip is not installed. The fatpacked executable will be really big."
 fi
 
 perl -e 'print "+++ perlbrew fatpacking with $]\n"'
@@ -26,4 +26,3 @@ chmod +x perlbrew
 mv ./perlbrew ../
 
 echo "+++ DONE"
-
