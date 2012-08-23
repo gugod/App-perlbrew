60c80a76f4a276ad6a0c47ceee6385c5eddcfa70
Kang-min Liu <gugod@gugod.org>
2012-08-23 19:34:09 +0200

    Replace the deprecate warning for `perlbrew install` with its usage.

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -782,18 +782,13 @@ sub do_install_release {
 
 sub run_command_install {
     my ( $self, $dist, $opts ) = @_;
-    $self->{dist_name} = $dist;
 
-    unless ($dist) {
-        ## Show a deprecation warning.
-        print STDERR "-" x 76 . "\n";
-        print STDERR "DEPRECATION WARNING:\n\n  Run `perlbrew install` to install perlbrew itself is deprecated. \n  Please run `perlbrew self-install` in the future instead.\n\n";
-        print STDERR "-" x 76 . "\n\n";
-        sleep 5;
-
-        $self->run_command_self_install();
-        return
+    unless($dist) {
+        $self->run_command_help("install");
+        exit -1;
     }
+
+    $self->{dist_name} = $dist;
 
     my $installation_name = $self->{as} || $dist;
     if ($self->is_installed( $installation_name ) && !$self->{force}) {
