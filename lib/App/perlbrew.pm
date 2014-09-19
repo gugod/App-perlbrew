ba0f9211c673309bbfa0acb14f7dfefd866b8a89
singularperturbation <sloanes.k@gmail.com>
2014-09-19 18:39:09 -0500

    Append PERLBREW_MANPATH with delimiter for fish

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -2589,6 +2589,7 @@ function __perlbrew_set_path
     set -l MANPATH_WITHOUT_PERLBREW (perl -e 'print join ":", grep { index($_, $ENV{PERLBREW_HOME}) < 0 } grep { index($_, $ENV{PERLBREW_ROOT}) < 0 } split/:/,qx(manpath 2> /dev/null);')
 
     if test -n "$PERLBREW_MANPATH"
+        set -l PERLBREW_MANPATH $PERLBREW_MANPATH":"
         set -x MANPATH {$PERLBREW_MANPATH}{$MANPATH_WITHOUT_PERLBREW}
     else
         set -x MANPATH $MANPATH_WITHOUT_PERLBREW
