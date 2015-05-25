9607010a21a5075e752962b3782294e9f9545e80
Yanick Champoux <yanick@babyl.dyndns.org>
2015-05-25 15:47:56 -0400

    Silence the warning
    Fish warns if a non-existant directory is added to $PATH.
    Which can happens for some installs of perlbrew.
    
    Fixes #446

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -2598,11 +2598,12 @@ function __perlbrew_set_path
 
     set -l PATH_WITHOUT_PERLBREW (eval $perlbrew_command display-pristine-path | perl -pe'y/:/ /')
 
+    # silencing stderr in case there's a non-existent path in $PATH (see GH#446)
     if test -n "$PERLBREW_PATH"
         set -x PERLBREW_PATH (echo $PERLBREW_PATH | perl -pe 'y/:/ /' )
-        eval set -x PATH $PERLBREW_PATH $PATH_WITHOUT_PERLBREW
+        eval set -x PATH $PERLBREW_PATH $PATH_WITHOUT_PERLBREW 2> /dev/null
     else
-        eval set -x PATH $PERLBREW_ROOT/bin $PATH_WITHOUT_PERLBREW
+        eval set -x PATH $PERLBREW_ROOT/bin $PATH_WITHOUT_PERLBREW 2> /dev/null
     end
 end
 
