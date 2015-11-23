b9a393373fe0c5f0a0aeb64b0c89fa33e1c4fbc7
Kang-min Liu <gugod@gugod.org>
2015-11-23 20:13:10 +0100

    [travis-ci] debugging test env failures.

--- dev-bin/travis-prepare.sh
+++ dev-bin/travis-prepare.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 yes | cpanm --verbose local::lib
 cpanm -n Module::Install Module::Install::AuthorRequires Devel::Cover Devel::Cover::Report::Coveralls
+cpanm --verbose --notest --installdeps .
