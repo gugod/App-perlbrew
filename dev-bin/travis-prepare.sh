4dd8dd3dd5c07cddc7b11040117d4ac56ae0a25c
Kang-min Liu <gugod@gugod.org>
2017-06-16 20:26:49 +0200

    [travis-ci]: stick with Devel::Cover 1.23 on older perl
    The reason for this is this dying message from Devel::Cover
    
        Devel::Cover 0.001 is not supported on perl 5.008009.  The last version of
        Devel::Cover which was supported was version 1.23.  This version may not work.
        I have not tested it.  If it does work it will not be fully functional.
    
    It is also stated in the Changes of Devel::Cover that old perls are unsupported:
    
        Release 1.24 - 18 April 2017
         - Work with 5.25.x (op_sibling) (Dan Collins, Matthew Horsfall) (github 162).
         - Perl versions below 5.10.0 are now unsupported.
    
    ref: https://metacpan.org/changes/distribution/Devel-Cover

--- dev-bin/travis-prepare.sh
+++ dev-bin/travis-prepare.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 yes | cpanm --verbose local::lib
-cpanm -n Pod::Markdown Module::Install Module::Install::AuthorRequires Devel::Cover Devel::Cover::Report::Coveralls
+
+if perl -e 'exit( ($] < 5.010) ? 0 : 1)'
+then
+    cpanm -n PJCJ/Devel-Cover-1.23.tar.gz
+else
+    cpanm -n Devel::Cover
+fi
+
+cpanm -n Pod::Markdown Module::Install Module::Install::AuthorRequires Devel::Cover::Report::Coveralls
 cpanm --verbose --notest --installdeps .
