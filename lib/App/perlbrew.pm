c8632d986cd041c7097730ae63378475d65cb2f4
Kang-min Liu <gugod@gugod.org>
2017-08-16 21:52:08 +0200

    Revert "remove this unused subroutine."
    This reverts commit aa42ab80d9ec1998d3a955e118b54986abf9f923.

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -912,6 +912,21 @@ sub release_detail_cperl_local {
     return ($error, $rd);
 }
 
+sub release_detail_cperl_remote {
+    my ($self, $dist, $rd) = @_;
+    $rd ||= {};
+    my $expect_href = "/perl11/cperl/archive/${dist}.tar.gz";
+    my $expect_url = "https://github.com/perl11/cperl/archive/${dist}.tar.gz";
+    my $html = http_get('https://github.com/perl11/cperl/tags');
+    my $error = 1;
+    if ($html =~ m{ <a \s+ href="$expect_href" }xsi) {
+        $rd->{tarball_name} = "${dist}.tar.gz";
+        $rd->{tarball_url}  = $expect_url;
+        $error = 0;
+    }
+    return ($error, $rd);
+}
+
 sub release_detail {
     my ($self, $dist) = @_;
     my ($dist_type, $dist_version, $tarball_name, $tarball_url);
