46c3f81c6f963cca062c13ec9b8ac5ccef0710c7
Kang-min Liu <gugod@gugod.org>
2017-12-18 08:11:53 +0900

    automate some part of making new releases.

---
+++ dev-bin/release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure we are at the right directory.
+cd $(dirname $0)/..
+
+CURRENT_VERSION=$(egrep 'our \$VERSION = "[\.0-9]+";' lib/App/perlbrew.pm  | egrep -o '[0-9]+' | tail -1)
+RELEASE_VERSION=0.$((CURRENT_VERSION+1))
+RELEASE_TIMESTAMP=$(date +'%FT%T%z')
+RELEASE_THANKS=$(git shortlog -s "$(git tag --list release-'*' | tail -1)"..HEAD | grep -v "$(git config user.name)" | perl -nE 'push@a,(split /[\t\n]/)[1]}{say join(", ",@a)')
+
+echo '# $VERSION++'
+(
+    cat lib/App/perlbrew.pm | sed 's/our $VERSION = "0.'${CURRENT_VERSION}'";/our $VERSION = "'${RELEASE_VERSION}'";/'
+) > lib/App/perlbrew.pm.new
+mv  lib/App/perlbrew.pm.new  lib/App/perlbrew.pm
+
+echo '# update Changes' 
+(
+    echo ${RELEASE_VERSION}: \# ${RELEASE_TIMESTAMP}
+    echo '- ' Thanks to our contributors: ${RELEASE_THANKS}
+    cat Changes
+) > Changes.new
+mv Changes.new Changes
+
+echo '# rebuild'
+./dev-bin/build.sh
+
+echo '# ' Now, check them:
+echo '> git status '
+git status
