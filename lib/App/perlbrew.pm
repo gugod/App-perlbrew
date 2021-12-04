0f66487ed67730ff9f97a25faef0cdf9a94b9f03
Kang-min Liu <gugod@gugod.org>
2021-12-05 08:35:45 +0900

    [self-upgrade]: show version numbers.
    Also, respect `--quiet` flag and be quiet.

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -2232,10 +2232,14 @@ sub run_command_self_upgrade {
     } else {
         die "Unable to detect version of new perlbrew!\n";
     }
+
     if ($new_version <= $VERSION) {
-        print "Your perlbrew is up-to-date.\n";
+        print "Your perlbrew is up-to-date (version $VERSION).\n" unless $self->{quiet};
         return;
     }
+
+    print "Upgrading from $VERSION to $new_version\n" unless $self->{quiet};
+
     system $TMP_PERLBREW, "self-install";
     $TMP_PERLBREW->unlink;
 }
