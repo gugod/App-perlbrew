dfba19ba9d50a42d79f5a2338040f610dc9caafe
Kang-min Liu <gugod@gugod.org>
2024-08-31 10:16:35 +0900

    replace stdout_from with the one from PerlbrewTestHelpers

--- t/PerlbrewTestHelpers.pm
+++ t/PerlbrewTestHelpers.pm
@@ -3,26 +3,24 @@ use Test2::V0;
 use Test2::Plugin::IOEvents;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(stdout_is stdout_like);
+our @EXPORT_OK = qw(stdout_from stdout_is stdout_like);
 
 # Replacements of Test::Output made by using Test2::Plugin::IOEvents
 
-sub capture_stdout {
+sub stdout_from (&) {
     my ($cb) = @_;
     my $events = intercept { $cb->() };
-
-
     return join "", map { $_->details } grep { $_->tag eq 'STDOUT' } map { @{ $_->facets->{info} } }@$events;
 }
 
 sub stdout_is (&$;$) {
     my ($cb, $expected, $desc) = @_;
-    is(capture_stdout($cb), $expected, $desc);
+    is(stdout_from(sub { $cb->() }), $expected, $desc);
 }
 
 sub stdout_like (&$;$) {
     my ($cb, $re, $desc) = @_;
-    like(capture_stdout($cb), $re, $desc);
+    like(stdout_from(sub { $cb->() }), $re, $desc);
 }
 
 
