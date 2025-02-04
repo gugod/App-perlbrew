754901eaf9e38126798347208fff777159be2457
Kang-min Liu <gugod@gugod.org>
2025-02-04 10:40:59 +0800

    On cygwin, require `--force-local -` parameter to `tar`.
    From: https://github.com/gugod/App-perlbrew/discussions/832
    
        The problem occurs where perlbrew acts on a downloaded perl
        archive, using gnu tar to expand it. Gnu tar must not be used on a
        filename that is absolute, on cygwin or MSWindows, because it
        misunderstands the C: as referring to a remote tape drive or
        similar device. The flag --force-local must be used or tar dies
        with an error.
    
    While I would very much like to see someone setup their C:\ being a
    tape drive, that would have to be the topic for those retro-PC
    youtubers, no perlbrew.
    
    Co-authored-by: Sören Andersen <somian08@gmail.com>

--- lib/App/perlbrew.pm
+++ lib/App/perlbrew.pm
@@ -1139,14 +1139,26 @@ sub do_extract_tarball {
     $workdir->mkpath;
     my $extracted_dir;
 
-    # Was broken on Solaris, where GNU tar is probably
-    # installed as 'gtar' - RT #61042
-    my $tarx = ( $^O =~ /solaris|aix/ ? 'gtar ' : 'tar ' )
-        . (
-          $dist_tarball =~ m/xz$/  ? 'xJf'
-        : $dist_tarball =~ m/bz2$/ ? 'xjf'
-        :                            'xzf'
-        );
+    my $tarx;
+    if ($^O eq 'cygwin') {
+        # https://github.com/gugod/App-perlbrew/issues/832
+        # https://github.com/gugod/App-perlbrew/issues/833
+        $tarx = 'tar --force-local -';
+    } elsif ($^O =~ /solaris|aix/) {
+        # On Solaris, GNU tar is installed as 'gtar' - RT #61042
+        # https://rt.cpan.org/Ticket/Display.html?id=61042
+        $tarx = 'gtar ';
+    } else {
+        $tarx = 'tar ';
+    }
+
+    if ($dist_tarball =~ m/xz$/) {
+        $tarx .= 'xJf';
+    } elsif ($dist_tarball =~ m/bz2$/) {
+        $tarx .= 'xjf';
+    } else {
+        $tarx .= 'xzf';
+    }
 
     my $extract_command = "cd $workdir; $tarx $dist_tarball";
     die "Failed to extract $dist_tarball" if system($extract_command);
