e5c4524d24f1993b5320847e5b113aa667fe518c
Kang-min Liu <gugod@gugod.org>
2025-06-07 08:44:34 +0900

    add a new module for probing the existence of patchperl

---
+++ lib/App/Perlbrew/Patchperl.pm
@@ -0,0 +1,40 @@
+package App::Perlbrew::Patchperl;
+use strict;
+use warnings;
+use 5.008;
+
+use App::Perlbrew::Path::Root;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(maybe_patchperl);
+
+sub maybe_patchperl {
+    my App::Perlbrew::Path::Root $perlbrew_root = shift;
+    return (
+        maybe_patchperl_in_app_root($perlbrew_root) ||
+        maybe_patchperl_in_system()
+    );
+}
+
+sub maybe_patchperl_in_app_root {
+    my App::Perlbrew::Path::Root $perlbrew_root = shift;
+    my $patchperl = $perlbrew_root->bin("patchperl");
+
+    if (-x $patchperl && -f _) {
+        return $patchperl;
+    } else {
+        return undef;
+    }
+}
+
+sub maybe_patchperl_in_system {
+    my $code = system("patchperl --version") >> 8;
+
+    if ($code != 127) {
+        return "patchperl"
+    } else {
+        return undef;
+    }
+}
+
+1;
